224107bbf27410c3e3879623e2dfeb98ce215e80
andrew <asc269@cornell.edu>
2023-01-16 14:01:29 -0800

    Initial Commit

---
+++ Central/CentralApp.swift
@@ -0,0 +1,17 @@
+//
+//  CentralApp.swift
+//  Central
+//
+//  Created by andrew choi on 1/16/23.
+//
+
+import SwiftUI
+
+@main
+struct CentralApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Central/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Central
+//
+//  Created by andrew choi on 1/16/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
